2c03f7a2915c5bf89693cad60953229b0fabcc01
stblake <samuel.thomas.blake@gmail.com>
2022-05-19 00:35:09 +1000

    Delete smooth.h

--- model/smooth.h
+++
@@ -1,8 +0,0 @@
-
-
-#include "common.h"
-#include "io.h"
-
-void smooth(float **unsmoothed, float **smoothed, int ncols, int nrows, 
-            float weight, int npasses, float spval);
-inline float verify(float gp, float spval, float weight, float *total);
